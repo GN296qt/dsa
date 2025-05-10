0e0b2cb0228778d090306a84e371f2e682481952
Gorli Gomati Ramya <g.gomatiramyaa@gmail.com>
2025-05-10 14:19:03 +0530

    narrowing

---
+++ Basic/datatype/conversion/narrowing.java
@@ -0,0 +1,10 @@
+package Basic.datatype.conversion;
+
+public class narrowing {
+      public static void main(String[] args) {
+          double num = 34.5;
+          int result = (int) num ;
+          System.out.println(" conversion from double to int "+ result);
+      }
+}
+//conversion from double to int 34
--- Basic/datatype/conversion/widening.java
+++ Basic/datatype/conversion/widening.java
@@ -3,6 +3,8 @@ package Basic.datatype.conversion;
 public class widening {
  public static void main(String[] args) {
      int num = 20 ;
-     System.out.println("conversion from int to double "+ num);
+     double result = num;
+     System.out.println("conversion from int to double "+ result);
  }
 }
+//conversion from int to double 20.0
