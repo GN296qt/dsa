c9ae216f0851dd588972dd6df4d4595d019107d9
Gorli Gomati Ramya <g.gomatiramyaa@gmail.com>
2025-05-10 12:15:43 +0530

    varaible and it's updation

---
+++ Basic/variable.java
@@ -0,0 +1,18 @@
+package Basic;
+
+public class variable {
+    public static void main(String[] args) {
+           int x ;
+           x = 500 ;
+           System.out.println(x);
+           x = 7;
+           System.out.println(x);
+           x = x + 6 ;
+           System.out.println(x); 
+           x =  x - 20;
+           System.out.println(x);
+           
+
+
+    }
+}
