0877299f2b5d12a07fa168aaf021145fe5a1c9de
Gorli Gomati Ramya <g.gomatiramyaa@gmail.com>
2025-05-10 10:56:54 +0530

    number  printing

--- Basic/printing/number.java
+++ Basic/printing/number.java
@@ -1,5 +1,10 @@
 package Basic.printing;
 
 public class number {
-    
+     public static void main(String[] args) {
+            System.out.println("4");
+            System.out.println(4);
+            System.out.println(4+3);
+            System.out.println("4+3");
+     }
 }
